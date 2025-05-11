1dcae1e6943e37051508fa5b7ec86eda5b13acf9
prajyotsolankure <prajyotsolankureof@gmail.com>
2025-05-11 16:06:31 +0530

    Create 27. Remove Element.java

---
+++ 27. Remove Element.java
@@ -0,0 +1,39 @@
+/*
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+
+class Solution {
+    public int removeElement(int[] a, int val) {
+        int j =0;
+        for(int i =0;i<a.length;i++){
+            if(val != a[i]){
+                a[j] = a[i];
+                j++;
+            }
+        }
+        return j;
+    }
+    public static void main(String args[]){
+        Scanner l = new Scanner(System.in);
+        int n = l.nextInt();
+        int[] a = new int[n];
+        int val = l.nextInt();
+        for(int i =0;i<n;i++){
+            a[i] = l.nextInt();
+        }
+        Solution sol = new Solution();
+        sol.removeElement(a,val);
+    }
+}
